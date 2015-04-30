43d08712d29e4b15b056e89bc7849bdedadf1c69
bleepbloopsify <bleepbloopsify@gmail.com>
2015-04-29 21:58:37 -0400

    Added Methods

--- bin/WordTree.java
+++ bin/WordTree.java
@@ -1,22 +1,48 @@
+import java.util.*;
+
 public class WordTree{
 
-    private WordTreeNode root;
+  private WordTreeNode root;
 
-    public WordTree(){
-	root = new WordTreeNode();
+  public WordTree(){
+    root = new WordTreeNode();
+  }
+
+  public void add(String word){
+    root = add(word, root.get(word.charAt(0)));
+  }
+
+  public WordTreeNode add(String word, WordTreeNode curr){
+    char c = word.charAt(0);
+    if (c == null){
+      curr.setWord(true);
+      return curr;
     }
+    else
+      if (curr.getLetter(c) == null)
+	curr.setLetter(c, new WordTreeNode(c));
+    curr.setLetter(c, add(word.substring(1,word.length()), curr.getLetter(c)));
+    return curr;
+  }
 
-    
+  public String getWord(){
+    return getWord(root);
+  }
 
-    private static void setFile(String file){
+  public String getWord(WordTreeNode curr){
+    if (curr.isWord())
+      return curr.getData()
+  }
 
-	BufferedReader br = new BufferedReader(new FileReader(file));
-	String init = br.readline();
-	while(init != 
+  private static void setFile(String file){
+
+    BufferedReader br = new BufferedReader(new FileReader(file));
+    String init = br.readline();
+    while(init != 
 
 
 
-    }
+	  }
 
     public static void main(String[] args){
 
@@ -24,4 +50,4 @@ public class WordTree{
 
 
 
-}
+  }
