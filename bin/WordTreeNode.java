c8ac84e1ade223c1f113b28e25f3af0a84a76252
captiosus <royxu652@gmail.com>
2015-05-10 17:19:01 -0400

    user input + readme

--- bin/WordTreeNode.java
+++ bin/WordTreeNode.java
@@ -2,85 +2,85 @@ import java.util.*;
 
 public class WordTreeNode{
 
-    private WordTreeNode[] letters;
-    private char data;
-    private boolean isWord;
+  private WordTreeNode[] letters;
+  private char data;
+  private boolean isWord;
 
-    public WordTreeNode(){
-	letters = new WordTreeNode[26];
-	isWord = false;
-    }
+  public WordTreeNode(){
+   letters = new WordTreeNode[26];
+   isWord = false;
+ }
 
-    public WordTreeNode(char c){
-	letters = new WordTreeNode[26];
-	isWord = false;
-	data = c;
-    }
+ public WordTreeNode(char c){
+   letters = new WordTreeNode[26];
+   isWord = false;
+   data = c;
+ }
 
-    public void setData(char c){
-	data = c;
-    }
+ public void setData(char c){
+   data = c;
+ }
 
-    public String toString(){
-	return data + "";
-    }
+ public String toString(){
+   return data + "";
+ }
 
-    public WordTreeNode get(String s){
-	char c = s.charAt(0);
-	if (s.length() == 1)
-	    return getLetter(c);
-	return letters[(int)c - 97].get(s.substring(1,s.length()));
-    }
+ public WordTreeNode get(String s){
+   char c = s.charAt(0);
+   if (s.length() == 1)
+     return getLetter(c);
+   return letters[(int)c - 97].get(s.substring(1,s.length()));
+ }
 
-    public ArrayList<WordTreeNode> getAll(){
-	ArrayList<WordTreeNode> filled = new ArrayList<WordTreeNode>();
-	for(int i = 0; i < letters.length; i++)
-	    if(letters[i] != null)
-		filled.add(letters[i]);
-	return filled;
-    }
+ public ArrayList<WordTreeNode> getAll(){
+   ArrayList<WordTreeNode> filled = new ArrayList<WordTreeNode>();
+   for(int i = 0; i < letters.length; i++)
+     if(letters[i] != null)
+      filled.add(letters[i]);
+    return filled;
+  }
 
-    public WordTreeNode getOneLetter(){
-	ArrayList<WordTreeNode> filled = new ArrayList<WordTreeNode>();
-	for(int i = 0; i < letters.length; i++)
-	    if(letters[i] != null)
-		filled.add(letters[i]);
-	return filled.get( (int)(Math.random() * filled.size()));
-    }
+  public WordTreeNode getOneLetter(){
+   ArrayList<WordTreeNode> filled = new ArrayList<WordTreeNode>();
+   for(int i = 0; i < letters.length; i++)
+     if(letters[i] != null)
+      filled.add(letters[i]);
+    return filled.get( (int)(Math.random() * filled.size()));
+  }
 
-    public boolean isLast(){
-	ArrayList<WordTreeNode> filled = new ArrayList<WordTreeNode>();
-	for(int i = 0; i < letters.length; i++)
-	    if(letters[i] != null)
-		filled.add(letters[i]);
-	return filled.size() == 0;
-    }
+  public boolean isLast(){
+   ArrayList<WordTreeNode> filled = new ArrayList<WordTreeNode>();
+   for(int i = 0; i < letters.length; i++)
+     if(letters[i] != null)
+      filled.add(letters[i]);
+    return filled.size() == 0;
+  }
 
-    public boolean isWord(){
-	return isWord;
-    }
+  public boolean isWord(){
+   return isWord;
+ }
 
-    public boolean hasLetter(char c){
-	return letters[(int)c - 97] == null;
-    }
+ public boolean hasLetter(char c){
+   return letters[(int)c - 97] == null;
+ }
 
-    public void setWord(boolean b){
-	isWord = b;
-    }
+ public void setWord(boolean b){
+   isWord = b;
+ }
 
-    public char getData(){
-	return data;
-    }
+ public char getData(){
+   return data;
+ }
 
-    public void setLetter(char c){
-	letters[ (int) c - 97 ] = new WordTreeNode(c);
-    }
-  
-    public void setLetter(char c, WordTreeNode w){
-	letters[ (int) c - 97 ] = w;
-    }
-  
-    public WordTreeNode getLetter(char c){
-	return letters[ (int)c - 97 ];
-    }
+ public void setLetter(char c){
+   letters[ (int) c - 97 ] = new WordTreeNode(c);
+ }
+ 
+ public void setLetter(char c, WordTreeNode w){
+   letters[ (int) c - 97 ] = w;
+ }
+ 
+ public WordTreeNode getLetter(char c){
+   return letters[ (int)c - 97 ];
+ }
 }
